b144b30209a5ef34e03af47a76e06e09e57fc9bd
youzhenghong <yzh19950927@gmail.com>
2018-05-07 16:17:00 -0700

    try to fix bugs

--- src/main/java/globesort/GlobeSortServer.java
+++ src/main/java/globesort/GlobeSortServer.java
@@ -86,7 +86,7 @@ public class GlobeSortServer {
 
         @Override
         public void sortIntegers(IntArray req, final StreamObserver<IntArray> responseObserver) {
-            System.out.println("ready to sort......size: " + Integer.getValuesCount());
+            System.out.println("ready to sort......size: " + req.getValuesCount());
             Double startTime = 0.0;
             Double endTime = 0.0;
             startTime = System.currentTimeMillis() * 1.0 / 1000.0;
