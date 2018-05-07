5c6ae9e5a7d997f99b3e5c1e338593a2f246c598
youzhenghong <yzh19950927@gmail.com>
2018-05-07 15:06:17 -0700

    fix bug

--- src/main/java/globesort/GlobeSortServer.java
+++ src/main/java/globesort/GlobeSortServer.java
@@ -97,7 +97,7 @@ public class GlobeSortServer {
             for(Integer val : values) {
                 responseBuilder.addValues(val);
             }
-            responseBuilder.addTime(timeElapsed);
+            responseBuilder.setTime(timeElapsed);
             IntArray response = responseBuilder.build();
             responseObserver.onNext(response);
             responseObserver.onCompleted();
